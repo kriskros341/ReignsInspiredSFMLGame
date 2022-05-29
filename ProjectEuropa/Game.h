dbf90e7958d54f089ce79219de587b7ddf7bfd0a
N!9r4 <KiniGra.665@gmail.com>
2022-05-29 14:08:35 +0200

    HELP 2

--- ProjectEuropa/Game.h
+++ ProjectEuropa/Game.h
@@ -1,55 +1,36 @@
-#pragma once
+﻿#pragma once
 #include <SFML/Graphics.hpp>
 #include <iostream>
-#include <string>
 #include "MyRenderWindow.h"
 
 extern sf::Font systemFont;
 extern sf::Vector2f screenSize;
 
 class Resource : public sf::RectangleShape {
-	int* value = 0;
+	int value; // please do not edit here
 	sf::Text t;
-	sf::Sprite s;
 	sf::Vector2f startingPosition;
-	sf::Texture resourceTexture;
 	friend class MyRenderWindow;
 public:
-<<<<<<< Updated upstream
 	Resource() {};
-	Resource(sf::Vector2f size, sf::Vector2f position) : sf::RectangleShape(size) {
-		t.setFont(systemFont);
-		t.setString(std::to_string(100));
-=======
-	Resource()  {};
-	Resource(sf::Vector2f size, sf::Vector2f position, int v, std::string texturePath) : sf::RectangleShape(size), value(v) {
+	Resource(sf::Vector2f size, sf::Vector2f position, int v) : sf::RectangleShape(size), value(v) {
 
-		/*t.setFont(systemFont);
+		t.setFont(systemFont);
 		t.setString(std::to_string(v));
 		startingPosition = position;
 		setPosition(startingPosition);
-
 		t.setFillColor(sf::Color::Magenta);
-
 		t.setOrigin(t.getLocalBounds().width / 2, t.getLocalBounds().height / 2);
-		t.setPosition(startingPosition.x + size.x / 2.0, startingPosition.y + size.y / 2.0);*/
-		
-		/*resourceTexture.loadFromFile(texturePath);
-		s.setTexture(resourceTexture);*/
-		
-		startingPosition = position;
-		setPosition(startingPosition);
-		s.setOrigin(s.getLocalBounds().width / 2, s.getLocalBounds().height / 2);
-		s.setPosition(startingPosition.x + size.x / 2.0, startingPosition.y + size.y / 2.0);
+		t.setPosition(startingPosition.x + size.x / 2.0, startingPosition.y + size.y / 2.0);
 	}
 	void setValue(int newval) {
 		value = newval;
 		t.setString(std::to_string(value));
+
 	};
 	int getValue() {
 		return value;
 	};
->>>>>>> Stashed changes
 };
 
 
@@ -58,23 +39,28 @@ float approx_linear(float input[2], float output[2], float value);
 class GUI : public sf::RectangleShape {
 	friend class MyRenderWindow;
 	Resource* resources;
-	const float resourceCount = 4;
+	const int resourceCount = 4;
 public:
-	GUI(sf::FloatRect rect, DecisionStats& stats) : sf::RectangleShape({rect.width, rect.height}) {
->>>>>>> Stashed changes
+	GUI(sf::FloatRect rect, DecisionStats& stats) : sf::RectangleShape({ rect.width, rect.height }) {
 		float width = rect.width, height = rect.height;
-		resources = new Resource[(int)resourceCount];
+		resources = new Resource[resourceCount];
+
 		for (int i = 0; i < resourceCount; i++) {
 			resources[i] = Resource(
-				{ width / resourceCount, height }, 
+				{ width / resourceCount, height },
 				{ rect.left + i * width / resourceCount, rect.top },
 				stats[i]
 			);
 
-			/*if(i % 2 == 0)
-				resources[i].setFillColor(sf::Color::Yellow);*/
+			if (i % 2 == 0)
+				resources[i].setFillColor(sf::Color::Yellow);
 		}
 	};
+	void updateBars(DecisionStats& s) {
+		for (int i{}; i < 4; i++) {
+			resources[i].setValue(s[i]);
+		}
+	}
 };
 
 float getAngleBetween(sf::Vector2f origin, sf::Vector2f theOther);
@@ -85,13 +71,12 @@ class MainCard : public sf::Sprite {
 	sf::FloatRect starting;
 	sf::Texture texture;
 public:
+
 	// width, height, left, top
-	MainCard(sf::FloatRect s, std::string texturePath) : sf::Sprite() {
+	MainCard(sf::FloatRect s) : sf::Sprite() {
 		setOrigin(s.width / 2.0, s.height / 2.0);
 		starting = s;
-		setPosition(screenSize.x / 2.0f, screenSize.y / 2.0f + 100.0f);
-		texture.loadFromFile(texturePath);
-		setTexture(texture);
+		setPosition(screenSize.x / 2.0, screenSize.y / 2.0 + 100.0f);
 	}
 	bool getDragging();
 	void dragHorizontally(sf::Vector2f position);
@@ -100,28 +85,23 @@ public:
 };
 
 class NextCard : public sf::Sprite {
-private:
-	sf::Texture texture;
 public:
-	NextCard(sf::FloatRect s, std::string texturePath) : sf::Sprite() {
-		setOrigin(s.width / 2.0, s.height / 2.0);
-		setPosition(screenSize.x / 2.0f, screenSize.y / 2.0f + 100.0f);
-		texture.loadFromFile(texturePath);
-		setTexture(texture);
+	NextCard() : sf::Sprite() {
+		setOrigin(250.0 / 2.0, 250.0 / 2.0);
 	}
 };
 
-<<<<<<< Updated upstream
-=======
 // Wszystkie decyzje
 class AllDecisions {
 public:
 	std::shared_ptr<Decision> currentDecision;
 	std::vector<std::shared_ptr<Decision>> decisionPool;
+
 };
 
->>>>>>> Stashed changes
 class PlayableArea : public sf::RectangleShape {
+	DecisionStats stats{ 100, 100, 100, 100 };
+	AllDecisions decision;
 	GUI gui;
 	MainCard card;
 	NextCard next;
@@ -129,44 +109,68 @@ class PlayableArea : public sf::RectangleShape {
 	sf::RectangleShape yesZone, noZone;
 	friend class Game;
 	friend class MyRenderWindow;
+	sf::Texture texture;
+	sf::Texture personTexture;
 public:
+	std::shared_ptr<Decision> getCurrentDecision() {
+		return decision.currentDecision;
+	}
+	void makeDecision(bool v) {
+		if (!decision.currentDecision) {
+			std::cout << "NO DECISION TO BE MADE" << std::endl;
+			return;
+		}
+		stats.apply(v ?
+			getCurrentDecision()->getYesDecision()->getChangeParameters() :
+			getCurrentDecision()->getNoDecision()->getChangeParameters()
+		);
+		stats.cout();
+		updateGUI();
+	}
+	void updateGUI() {
+		gui.updateBars(stats);
+		//gui.
+	}
 	//PlayableArea(sf::FloatRect rect) :
 	PlayableArea(float width, float guiOffset) :
 		sf::RectangleShape({ width, screenSize.y - guiOffset }),
-<<<<<<< Updated upstream
-		gui({ screenSize.x / 2.0f - width / 2.0f, 0, width, guiOffset }),
-		card({ screenSize.x / 2.0f, screenSize.y / 2.0f + 100.0f, 250, 250 })
-=======
 		gui({ screenSize.x / 2.0f - width / 2.0f, 0, width, guiOffset }, stats),
 		card({ screenSize.x / 2.0f, screenSize.y / 2.0f + 100.0f, 250, 250 })
 	{
-	sf::FloatRect rect = { screenSize.x / 2.0f - width / 2.0f, 0, width, screenSize.y };
-	sf::Vector2f size = getSize();
-	gui.setPosition(rect.left, 0);
+		sf::FloatRect rect = { screenSize.x / 2.0f - width / 2.0f, 0, width, screenSize.y };
+		sf::Vector2f size = getSize();
+		gui.setPosition(rect.left, 0);
 
-	//CENTER
-	setPosition(rect.left, guiOffset);
+		next.setPosition(screenSize.x / 2.0, screenSize.y / 2.0 + 100.0f);
+		//next.setFillColor(sf::Color::Magenta);
 
-	yesZone.setSize({ 100, screenSize.y });
-	noZone.setSize({ 100, screenSize.y });
+		texture.loadFromFile("./assets/backCard250x250.png");
+		personTexture.loadFromFile("./assets/captainRed.png");
+		next.setTexture(texture);
+		card.setTexture(personTexture);
 
-	yesZone.setPosition(rect.left + rect.width, 0);
-	noZone.setPosition(rect.left - 100, 0);
-	yesZone.setFillColor(sf::Color{ 48, 42, 39 });
-	noZone.setFillColor(sf::Color{ 48, 42, 39 });
-	setFillColor(sf::Color{160, 148, 133});
+		//CENTER
+		setPosition(rect.left, guiOffset);
+
+		yesZone.setSize({ 100, screenSize.y });
+		noZone.setSize({ 100, screenSize.y });
+
+		yesZone.setPosition(rect.left + rect.width, 0);
+		noZone.setPosition(rect.left - 100, 0);
+		yesZone.setFillColor(sf::Color{ 48, 42, 39 });
+		noZone.setFillColor(sf::Color{ 48, 42, 39 });
+		setFillColor(sf::Color{ 160, 148, 133 });
 	}
 
 };
 
+//Obiekt z kt�rym gracz wchodzi w kontakt
 class Game {
-	AllDecisions decision;
 	PlayableArea area;
 	friend class MyRenderWindow;
-	DecisionStats statystyki{100, 100, 100, 100};
 public:
 	Game() : area(400, 150) {};
-	
+
 	bool doesIntersectWithMainCard(sf::FloatRect position);
 	bool doesIntersectWithMainCard(sf::Vector2f position);
 	void setDragging(bool n);
@@ -174,4 +178,10 @@ public:
 	bool getDragging();
 	void dragCard(sf::Vector2f position);
 	void makeChoice();
+	void setStartingDecision(std::shared_ptr<Decision>& o) {
+		area.decision.currentDecision = o;
+	}
+	void setDecisionPool(std::vector<std::shared_ptr<Decision>>& o) {
+		area.decision.decisionPool = o;
+	}
 };
